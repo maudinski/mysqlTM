bc597969b88aef134a24aa6a58dae5d747c1c379
maudinski <mauriciomartinez0707@gmail.com>
2017-07-04 17:46:41 -0500

    Comments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mysqlTM
 This is a package for abstractly managing insertions/deletions/verifications on mysql tables for Go
+Incomplete rn but what's here works
--- tm.go
+++ tm.go
@@ -1,59 +1,87 @@
 package mysqlTM
-//TODO need to handle sqlinjections and shit
-//TODO this package assumes all fields in the mysql table are string related (need ' '
-//around the queries)
-//TODO need to make/find a package for encrypting shit (maybe crypto)
-//custom error type would be awesome
+
+//Package for maintaing a table in mysql. Geared towards account management. Not complete
+//but whats here works
 import (
-	_"github.com/go-sql-driver/mysql"
 	"database/sql"
-	"fmt"
 	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type TableManager struct {
+	//pointer to the mysql database
 	db *sql.DB
+	//mysql query string
+	//"select (verifierField) from (table) where (verifierField) = (verifier)"
+	//for verifying things like sign ins, used in tm.Verify()
 	verifyQ string
+	//mysql query string.
+	//"insert into (table)(field, field...)". The "values( ... )" part is added
+	//dyanamically right before execution. used in tm.Insert()
 	insertQ string
+	//holds the amount of fields entered with the constructor (the ...string). Used
+	//to verify that enough fields where entered when tm.Insert() is called.
 	fieldAmt int
+	//name of the table that is being managed. This package could potentially manage
+	//many tables, but probably best not to
 	tableName string
+	//the field name of whatever unique identifier (optional, set with tm.SetupVerify).
+	//probably a username or an email.
 	unique string
+	//the field name of whatever the verifier for the unique name is(also optional ^^).
+	//mostlikely a password
 	verifier string
+	//the fields entered at constructor time
 	fields []string
 }
 
-func NewTM(dbUserName string, dbPass string, host string, db string, 
-						tableName string, fields ...string) (*TableManager, error) {
+//Returns a new TableManager object. Parameters- dbUserName: the username for mysql.
+//dbPass: password for that username. host: the host and port you will be connecting to
+//(pass empty string "" for the default). db: The name of the database that you're
+//connecting to. tableName: the name of the table that will be managed. fields: the
+//of that table, as they appear on that table. For now(this sucks), those fields must not
+//by any type of number. They must be strings or things that mysql accepts as strings (ie
+//datetime, time, varchar, char, etc). Will be changed later to be more dynamic
+func NewTM(dbUserName string, dbPass string, host string, db string,
+	tableName string, fields ...string) (*TableManager, error) {
 	var err error
 	tm := new(TableManager)
-	sqlopenString := dbUserName+":"+dbPass+"@"+host+"/"+db
+	sqlopenString := dbUserName + ":" + dbPass + "@" + host + "/" + db
 	tm.db, err = sql.Open("mysql", sqlopenString)
-	if err != nil { 
-		return nil, err 
+	if err != nil {
+		return nil, err
 	}
-	if len(fields) < 1{
-		return nil, errors.New("no tables fields entered")	
+	if len(fields) < 1 {
+		return nil, errors.New("no tables fields entered")
 	}
 	tm.fields = fields
 	tm.fieldAmt = len(fields)
 	tm.tableName = tableName
-	tm.insertQ = "insert into "+tableName+"("+seperateWithCommas(fields, false)+") "
-	tm.verifyQ = "select %v from "+tableName+" where %v = '%v'"
+	tm.insertQ = "insert into " + tableName + "(" + seperateWithCommas(fields, false) + ") "
+	tm.verifyQ = "select %v from " + tableName + " where %v = '%v'"
 	tm.unique = ""
 	tm.verifier = ""
 	return tm, nil
-
 }
-func (tm *TableManager) SetupVerify(uniqueFieldName string, 
-														verifierFieldName string) error {
-	//check that they exist in tm.fields
+
+//Sets up for tm.Verify() to work, otherwise tm.Verify() will return an error. Both
+//parameters are the names of the fields as they are in mysql, and as you entered them
+//for the constructor (ie: "username" for uniqueFieldName and "password" for
+//"verifierFieldName")
+func (tm *TableManager) SetupVerify(uniqueFieldName string,
+	verifierFieldName string) error {
+	//TODO check that they exist in tm.fields
 	tm.unique = uniqueFieldName
 	tm.verifier = verifierFieldName
 	return nil
 }
 
+//verifies that the unique field has a matching verifier (username matches password)
+//. returns true if it does, false if it doesnt, error if incorrect input was entered
+//or something went wrong with the database
 func (tm *TableManager) Verify(unique string, verifier string) (bool, error) {
-				
+
 	if tm.verifier == "" || tm.unique == "" {
 		return false, errors.New("Verifier or Unqiue table fields not set. " +
 			"Call tm.SetVerifier(verifierFieldName) and tm.SetUnique(uniqueFieldName)")
@@ -67,29 +95,32 @@ func (tm *TableManager) Verify(unique string, verifier string) (bool, error) {
 	return recievedVerifier == verifier, nil
 
 }
-//This shit is obviously not very efficient, but how the fuck can you write a completely
-//dynamic package that can take in variable amounts of arguments, and still be efficient?
-//you gotta do a bunch of work-arounds
-//TODO error check the lengths of shit, incase fuckers get clever
-func (tm *TableManager) Insert(fieldsInOrder ...string) error{	
-	
+
+//Inserts a new entry into the table. fieldsInOrder are the values to be inserted, in
+//the order that the fields were entered in the constructor. returns error if something
+//went wrong with the database
+func (tm *TableManager) Insert(fieldsInOrder ...string) error {
+
 	if len(fieldsInOrder) != tm.fieldAmt {
-		return errors.New("Enter a value for all fields")	
+		return errors.New("Enter a value for all fields")
 	}
-	//check if it exists
+
+	//TODO error check the lengths of shit, incase fuckers get clever
+	//TODO check if it exists CheckExits() down low
 	fullQuery := tm.insertQ + "values(" + seperateWithCommas(fieldsInOrder, true) + ");"
 	_, err := tm.db.Exec(fullQuery)
-	if err != nil{
-		return err	
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 //idgaf how disgusting this is
-func seperateWithCommas(strings []string, wrap bool) string {	
+//properly seperates shit for the query strings. wrap just wraps shit like this: 'shit'
+func seperateWithCommas(strings []string, wrap bool) string {
 	var s string
 	if wrap {
-		s = "'"+strings[0]+"'"
+		s = "'" + strings[0] + "'"
 	} else {
 		s = strings[0]
 	}
@@ -97,51 +128,41 @@ func seperateWithCommas(strings []string, wrap bool) string {
 		if wrap {
 			s += ", '" + strings[i] + "'"
 		} else {
-			s += ", " + strings[i]	
+			s += ", " + strings[i]
 		}
 	}
 	return s
 }
 
-func Index(strings []string, match string) (int, error) {
-	for i, str := range strings {
-		if str == match {
-			return i, nil
-		}	
-	}	
-	return -1, errors.New("not found")
-}
 /*********************some future functions*************************/
-func escapeSequence (str string) string{
+func escapeSequence(str string) string {
 	return str
 }
 
-func updateEntry(field string, newValue string) error {
-	return nil	
+//not written
+func UpdateEntry(field string, newValue string) error {
+	return nil
 }
 
+//not written
 func (tm *TableManager) CheckExists(uniqueField string, uniqueValue string) (bool, error) {
-	return false, nil			
+	return false, nil
 }
 
 func encrypt(str string) string {
-	return str	
+	return str
 }
 
 func decrypt(str string) string {
-	return str	
+	return str
 }
 
+//not written
 func (tm *TableManager) RemoveByUnique(unique string) error {
 	return errors.New("this function isnt written")
 }
 
+//not written
 func (tm *TableManager) Remove(fieldValues ...string) error {
-	return errors.New("this function isnt written")	
+	return errors.New("this function isnt written")
 }
-
-
-
-
-
-
